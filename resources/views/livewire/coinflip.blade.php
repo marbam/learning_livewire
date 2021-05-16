612eda157d918508d995abf3ceea60da1213e06c
marbam <martinbampton@hotmail.com>
2021-05-16 11:37:01 +0100

    Bit of styling to tidy up.

--- resources/views/livewire/coinflip.blade.php
+++ resources/views/livewire/coinflip.blade.php
@@ -1,16 +1,26 @@
 <div>
-    coinflip!
+    <h1 class="text-2xl">coinflip!</h1>
     <button wire:click="flip" type="button" class="bg-gray-400 p-2 rounded-lg">Flip!</button>
-    <button wire:click="auto" type="button" class="bg-gray-400 p-2 rounded-lg">Auto!</button>
-
+    <button wire:click="auto" type="button" class="bg-gray-400 p-2 rounded-lg">Auto until 10 Streak!</button>
 
     <p> Number of Heads: {{isset($results_split[0]) ? $results_split[0] : 0}}</p>
     <p> Number of Tails: {{isset($results_split[1]) ? $results_split[1] : 0}}</p>
     <p> Number of Flips: {{$number_of_flips}} </p>
     <p> Streak: {{$streak}} </p>
 
-    @foreach($last_ten as $res)
-        <p>{{$res}} - {{$streak}} </p>
-    @endforeach
+    @if ($last_ten)
+        <table>
+            <thead>
+                <th>Last Ten:</th>
+            </thead>
+            <tbody>
+                @foreach($last_ten as $res)
+                    <tr>
+                        <td>{{$res == 0 ? 'Heads' : 'Tails'}}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
 
 </div>
